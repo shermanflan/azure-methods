0e1c381d78a1049b7ee877f09fc83af6806b19e1
Ricardo Guzman <rrguzman1976@hotmail.com>
2018-05-24 15:55:09 -0400

    D365!

--- AzureOAuthClient/Program.cs
+++ AzureOAuthClient/Program.cs
@@ -15,6 +15,7 @@ namespace AzureOAuthClient
     {
         private static string aadInstance = ConfigurationManager.AppSettings["AADInstance"];
         private static string tenant = ConfigurationManager.AppSettings["Tenant"];
+        private static string tenant2 = ConfigurationManager.AppSettings["Tenant2"];
 
         private static string clientid = ConfigurationManager.AppSettings["app1:ClientId"];
         private static string redirectUri = ConfigurationManager.AppSettings["app1:RedirectUri"];
@@ -40,9 +41,21 @@ namespace AzureOAuthClient
         private static string APIVersion4 = ConfigurationManager.AppSettings["app4:ApiVersion"];
         private static string APIEndpoint4 = ConfigurationManager.AppSettings["app4:APIBaseAddress"];
 
+        private static string clientid5 = ConfigurationManager.AppSettings["app5:ClientId"];
+        private static string appKey5 = ConfigurationManager.AppSettings["app5:AppKey"];
+        private static string redirectUri5 = ConfigurationManager.AppSettings["app5:RedirectUri"];
+        private static string APIResourceId5 = ConfigurationManager.AppSettings["app5:APIResourceId"]; // target API
+        private static string APIEndpoint5 = ConfigurationManager.AppSettings["app5:APIBaseAddress"];
+
+        private static string clientid6 = ConfigurationManager.AppSettings["app6:ClientId"];
+        private static string redirectUri6 = ConfigurationManager.AppSettings["app6:RedirectUri"];
+        private static string APIResourceId6 = ConfigurationManager.AppSettings["app6:APIResourceId"]; // target API
+        private static string APIEndpoint6 = ConfigurationManager.AppSettings["app6:APIBaseAddress"];
+
         public static void Main(string[] args)
         {
             string authority = String.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
+            string authority2 = String.Format(CultureInfo.InvariantCulture, aadInstance, tenant2);
 
             try
             {
@@ -51,8 +64,8 @@ namespace AzureOAuthClient
                 //Console.WriteLine($"GetUser: {api.GetUser("rrguzman1976")}");
                 //Console.WriteLine($"WhoAmI: {api.WhoAmI()}");
 
-                DaemonKeyGraphAPI api2 = new DaemonKeyGraphAPI(authority, tenant, clientid2, appKey, APIResourceId2, APIVersion2, APIEndpoint2);
-                Console.WriteLine($"GetUser: {DaemonKeyGraphAPI.GetUser("Ricardo").Result}");
+                //DaemonKeyGraphAPI api2 = new DaemonKeyGraphAPI(authority, tenant, clientid2, appKey, APIResourceId2, APIVersion2, APIEndpoint2);
+                //Console.WriteLine($"GetUser: {DaemonKeyGraphAPI.GetUser("Ricardo").Result}");
 
                 //DaemonCertGraphAPI api3 = new DaemonCertGraphAPI(authority, tenant, clientid3, certName, App3APIResourceId, App3APIVersion, App3ApiEndpoint);
                 //Console.WriteLine($"GetUser: {api3.GetUser("Ric").Result}");
@@ -60,6 +73,12 @@ namespace AzureOAuthClient
                 //DaemonKeySQLAPI api4 = new DaemonKeySQLAPI(authority, tenant, clientid4, appKey4, APIResourceId4, APIVersion4, APIEndpoint4);
                 //Console.WriteLine($"Get Databases:\n{api4.GetDatabases().Result}");
 
+                DaemonKeyD365API api5 = new DaemonKeyD365API(authority2, tenant2, clientid5, appKey5, APIResourceId5, APIEndpoint5);
+                //Console.WriteLine($"Companies:\n{api5.GetAllCustomers().Result}");
+                Console.WriteLine($"Jobs:\n{api5.GetAllJobs().Result}");
+
+                //DaemonAutoD365API api6 = new DaemonAutoD365API(authority2, tenant2, clientid6, redirectUri6, APIResourceId6, APIEndpoint6);
+                //Console.WriteLine($"Get Company: {api6.GetCustomer("Contoso Europe")}");
             }
             catch (Exception e)
             {
