0e21312418a4b26bab0b939dcfd073a57abdaf1f
shermanflan <shermanflan@gmail.com>
2020-06-30 13:05:01 -0500

    Added YAML config

--- CDS/PythonClient/scripts/az_container.sh
+++ CDS/PythonClient/scripts/az_container.sh
@@ -1,18 +1,19 @@
 #!/bin/bash -eux
 
-declare CONTAINER="cds-python"
-declare RESOURCE_GROUP=""
-declare SUBSCRIPTION=""
-declare REGISTRY=""
+declare CONTAINER="pah-cds-python"
+declare RESOURCE_GROUP=
+declare SUBSCRIPTION=
+declare REGISTRY=
 declare LOCATION="East US"
 declare IMAGE="cdspython:1.0"
-declare ENV_TENANT=""
-declare ENV_CLIENT=""
-declare ENV_RESOURCE=""
-declare ENV_SECRET=""
-declare REGISTRY_URL=""
-declare REGISTRY_USER=""
-declare REGISTRY_PWD=""
+declare ENV_TENANT=
+declare ENV_CLIENT=
+declare ENV_RESOURCE=
+declare ENV_SECRET=
+declare REGISTRY_URL=
+declare REGISTRY_USER=
+declare REGISTRY_PWD=
+declare YAML_CONFIG="./cds-python.yml"
 
 # Create new ACR
 #az acr create --resource-group $RESOURCE_GROUP \
@@ -32,7 +33,6 @@ declare REGISTRY_PWD=""
 #docker tag python-cds $REGISTRY_URL/$IMAGE
 #docker push $REGISTRY_URL/$IMAGE
 
-# TODO: Can be defined using YAML file.
 # Create new container group
 #az container create \
 #  --resource-group $RESOURCE_GROUP \
@@ -53,8 +53,21 @@ declare REGISTRY_PWD=""
 #  --registry-username $REGISTRY_USER \
 #  --registry-password $REGISTRY_PWD
 
+# Alternative using YAML file.
+#az container create \
+#  --resource-group $RESOURCE_GROUP \
+#  --file $YAML_CONFIG
+
 # Start container instance
-az container start -n $CONTAINER \
-  --resource-group $RESOURCE_GROUP \
-  --subscription $SUBSCRIPTION \
-  --verbose --debug
+#az container start -n $CONTAINER \
+#  --resource-group $RESOURCE_GROUP \
+#  --subscription $SUBSCRIPTION \
+#  --verbose --debug
+
+# Start using Powershell
+#Invoke-AzureRmResourceAction `
+#  -ResourceGroupName $RESOURCE_GROUP `
+#  -ResourceName $CONTAINER `
+#  -Action Start `
+#  -Force `
+#  -ResourceType Microsoft.ContainerInstance/containerGroups
