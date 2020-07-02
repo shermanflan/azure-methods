1eb1f9c7f35f26683074a3dbb3e931a150761849
shermanflan <shermanflan@gmail.com>
2020-07-02 15:57:49 -0500

    Updated scripts

--- CDS/PythonClient/main.py
+++ CDS/PythonClient/main.py
@@ -238,8 +238,6 @@ if __name__ == "__main__":
         driver, server, database, username, password
     )
 
-    hello_odbc(cnxn_str)
-
     api_version = '9.1'
     request_url = f"{base_resource}/api/data/v{api_version}"
 
@@ -265,19 +263,19 @@ if __name__ == "__main__":
     #             by_filter="pah_name eq 'Saitama Sensei'")
 
     # PATCH pah_patient
-    # payload = {
-    #     "emailaddress": "johnny5@nomail.com",
-    #     "pah_address1_telephone1": "3241112222",
-    #     "pah_address1_line1": f"Address1-{datetime.now().isoformat()}",
-    #     "pah_address1city": f"Marfa-{datetime.now().isoformat()}",
-    #     "pah_address1_stateorprovince": f"Texas",
-    #     "pah_address1_postalcode": f"79757",
-    #     "pah_birthdate": datetime(year=2020, month=5, day=17).isoformat(),
-    #     "pah_gender": "804150000",
-    # }
-    # update_patient(base_url=request_url, headers=common_headers,
-    #                by_filter="pah_name eq 'Saitama Sensei'",
-    #                data=payload)
+    payload = {
+        "emailaddress": "johnny5@nomail.com",
+        "pah_address1_telephone1": "3241112222",
+        "pah_address1_line1": f"Address1-{datetime.now().isoformat()}",
+        "pah_address1city": f"Marfa-{datetime.now().isoformat()}",
+        "pah_address1_stateorprovince": f"Texas",
+        "pah_address1_postalcode": f"79757",
+        "pah_birthdate": datetime(year=2020, month=5, day=17).isoformat(),
+        "pah_gender": "804150000",
+    }
+    update_patient(base_url=request_url, headers=common_headers,
+                   by_filter="pah_name eq 'Saitama Sensei'",
+                   data=payload)
 
     # POST pah_patient
     # payload = {
@@ -293,5 +291,7 @@ if __name__ == "__main__":
     # DELETE pah_patient
     # delete_patient(base_url=request_url, headers=common_headers,
     #                by_filter="pah_name eq 'Saitama Sensei2'")
+
+    hello_odbc(cnxn_str)
 
     logger.info(f"Processing complete...")
--- CDS/PythonClient/scripts/az_container.sh
+++ CDS/PythonClient/scripts/az_container.sh
@@ -13,7 +13,12 @@ declare ENV_SECRET=""
 declare REGISTRY_URL=""
 declare REGISTRY_USER=""
 declare REGISTRY_PWD=""
-declare YAML_CONFIG="./cds-python.yml"
+declare YAML_CONFIG=~/Personal/github/azure-methods/CDS/PythonClient/scripts/cds-python.yml
+declare DB_DRIVER="{ODBC Driver 17 for SQL Server}"
+declare DB_SERVER=""
+declare DB=""
+declare DB_USER=""
+declare DB_PWD=""
 
 # Create new ACR
 #az acr create --resource-group $RESOURCE_GROUP \
@@ -38,15 +43,20 @@ declare YAML_CONFIG="./cds-python.yml"
 #  --resource-group $RESOURCE_GROUP \
 #  --name $CONTAINER \
 #  --image $REGISTRY_URL/$IMAGE \
-#  -l $LOCATION \
+#  -l "$LOCATION" \
 #  --os-type Linux --cpu 1 --memory 1.5 \
 #  --restart-policy Never \
 #  --environment-variables \
 #    AAD_TENANT=$ENV_TENANT \
 #    AAD_CLIENT=$ENV_CLIENT \
 #    AAD_RESOURCE=$ENV_RESOURCE \
+#    DB_DRIVER="$DB_DRIVER" \
+#    DB_SERVER=$DB_SERVER \
+#    DB=$DB \
 #  --secure-environment-variables \
-#    AAD_SECRET=$ENV_RESOURCE \
+#    AAD_SECRET=$ENV_SECRET \
+#    DB_USER=$DB_USER \
+#    DB_PWD="$DB_PWD" \
 #  --ip-address Private \
 #  --subscription $SUBSCRIPTION \
 #  --registry-login-server $REGISTRY_URL \
