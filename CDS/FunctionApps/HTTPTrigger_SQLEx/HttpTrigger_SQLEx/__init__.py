5f0a27593304881afccd94b8f998cfba188fdb6f
shermanflan <shermanflan@gmail.com>
2020-07-07 09:36:27 -0500

    Added asyncio

--- CDS/FunctionApps/HTTPTrigger_SQLEx/HttpTrigger_SQLEx/__init__.py
+++ CDS/FunctionApps/HTTPTrigger_SQLEx/HttpTrigger_SQLEx/__init__.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 import json
 import os
@@ -6,10 +7,56 @@ import azure.functions as func
 import pyodbc
 
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+async def get_meds(cnxn_str, name, limit=500):
+    """
+    Get a list of meds.
+    
+    Enhancements:
+    - Not sure async is working.
+
+    :param cnxn_str: SQL connection
+    :param name: name filter
+    :param limit: limit filter
+    :return: JSON string
+    """
+
+    try:
+
+        with pyodbc.connect(cnxn_str, autocommit=True) as cnxn:
+            cursor = cnxn.cursor()
+
+            qry1 = """
+                SELECT  ID
+                        , NAME
+                        , DOSE
+                FROM    [dbo].[GlobalMedList] WITH (READUNCOMMITTED)
+                WHERE   NAME LIKE ?
+                ORDER BY ID
+                OFFSET 0 ROWS FETCH NEXT ? ROWS ONLY
+                FOR JSON PATH;
+            """    
+            
+            # Retrieve single JSON object
+            result = cursor.execute(qry1, name+'%', int(limit))
+            payload = result.fetchone() 
+        
+            return payload[0]
+
+    except pyodbc.OperationalError as e:
+        logging.error(f"{e.args[1]}")
+        raise                        
+    finally:
+        pass
+
+
+async def main(req: func.HttpRequest): 
     """
     Inspired by:
     https://github.com/Azure-Samples/azure-sql-db-python-rest-api/blob/master/app.py
+    https://docs.microsoft.com/en-us/azure/azure-functions/functions-reference-python#async
+
+    :param req: the HttpRequest
+    :return: HttpResponse
     """
 
     logging.info('Python HTTP trigger function processed a request.')
@@ -22,38 +69,13 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         logging.debug(f"TEST PARAM: {name}")
         logging.debug(f"TEST PARAM: {limit}")
 
-        try:
+        payload = await get_meds(cnxn_str, name, limit)
 
-            # This calls cnxn.commit() when going out of scope.
-            with pyodbc.connect(cnxn_str, autocommit=False) as cnxn:
-                cursor = cnxn.cursor()
+        response = func.HttpResponse(body=payload, 
+                                        status_code=200,
+                                        mimetype='application/json')
 
-                qry1 = """
-                    SELECT  ID
-                            , NAME
-                            , DOSE
-                    FROM    [dbo].[GlobalMedList] WITH (READUNCOMMITTED)
-                    WHERE   NAME LIKE ?
-                    ORDER BY ID
-                    OFFSET 0 ROWS FETCH NEXT ? ROWS ONLY
-                    FOR JSON PATH;
-                """    
-                
-                # Retrieve single JSON object
-                result = cursor.execute(qry1, name+'%', int(limit))
-                payload = result.fetchone()[0] 
-            
-                response = func.HttpResponse(body=payload, 
-                                             status_code=200,
-                                             mimetype='application/json')
-
-            return response
-
-        except pyodbc.OperationalError as e:
-            logging.error(f"{e.args[1]}")
-            raise                        
-        finally:
-            pass
+        return response
     else:
         func.HttpResponse(json.dumps({'Bad Request': 'Parameters missing.'}), 
                           status_code=400,
