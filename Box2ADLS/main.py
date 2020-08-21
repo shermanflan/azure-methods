11be70e7fa2ab9688d396463210443cba5a6fa8a
shermanflan <shermanflan@gmail.com>
2020-08-20 22:42:54 -0500

    Refactor

--- Box2ADLS/main.py
+++ Box2ADLS/main.py
@@ -12,22 +12,55 @@ from box2adls.util.box_lib import (check_or_create_collab, navigate,
                                    download_files)
 
 
+def box_to_adls(box_client, source, adls_client, adls_root, target):
+    """
+    Routine which copies folders from Box to ADLS.
+    
+    :param box_client: box client with access to source folder
+    :param source: box source path
+    :param adls_client: ADLS client
+    :param adls_root: ADLS container
+    :param target: ADLS target path
+    :return: None
+    """
+    folders = source.split('/')[-1::-1]
+
+    logger.info(f"Navigating Box folder path '{'/'.join(folders)}'...")
+
+    box_root = box_client.folder('0').get()
+
+    logger.debug(f'Box root "{box_root.name}"')
+
+    daily = navigate(box_root, folders)
+
+    if not daily:
+        raise Exception(f'Daily folder "{daily}" not found.')
+
+    with TemporaryDirectory() as tmp_dir:
+
+        logger.info(f'Downloading Box files from "{daily.name}"...')
+
+        local_paths = download_files(box_folder=daily, local_dir=tmp_dir)
+
+        logger.info(f'Uploading to ADLS "{target}"...')
+
+        upload_files(adls_client, adls_root, adls_dir=target,
+                     files=local_paths)
+
+
 if __name__ == '__main__':
 
     box_user_id = environ.get('BOX_USER_ID', '')
     box_folder_id = environ.get('BOX_FOLDER_ID', '')
-    daily_folder = date.today().strftime('%d-%B')
-    box_folder_path = join(environ.get('BOX_FOLDER_PATH',
-                                       'data_source_01/folder_1/2020'),
-                           f"{daily_folder}")
-    folder_list = box_folder_path.split('/')[-1::-1]
     adls_account_name = environ.get('ADLS_ACCOUNT_NAME', 'pahintegrationstorage')
     adls_account_key = environ.get('ADLS_ACCOUNT_KEY',
-                                  '')
+                                   '')
     adls_url = f'https://{adls_account_name}.dfs.core.windows.net/'
-    adls_container_name = environ.get('ADLS_CONTAINER_NAME', 'box-demo-01')
-    adls_folder_path = environ.get('ADLS_FOLDER_PATH', 'fusion/box_downloads')
-    upload_dir = join(adls_folder_path, daily_folder)
+    adls_container = environ.get('ADLS_CONTAINER_NAME', 'box-demo-01')
+    adls_path = environ.get('ADLS_FOLDER_PATH', 'fusion/box_downloads')
+
+    daily_folder = date.today().strftime('%d-%B')
+    adls_dir = join(adls_path, daily_folder)
 
     logger.info("Authenticating to Box API...")
 
@@ -38,39 +71,20 @@ if __name__ == '__main__':
 
     logger.info("Authenticating to ADLS...")
 
-    adls_client = DataLakeServiceClient(account_url=adls_url,
-                                        credential=adls_account_key)
-
-    service_user = box_client_app.user().get()
-    logger.debug(f"Box Service: {service_user.name}")
+    adls_service = DataLakeServiceClient(account_url=adls_url,
+                                         credential=adls_account_key)
 
     # To access box folders, collaborations must be in place.
-    root_folder = box_client_user.folder(folder_id=box_folder_id).get()
+    check_or_create_collab(box_user_client=box_client_user,
+                           box_service_client=box_client_app,
+                           box_folder_id=box_folder_id)
 
-    logger.info(f"Verifying Box collaborations on '{root_folder.name}'")
+    box_dir = join(environ.get('BOX_FOLDER_PATH',
+                               'data_source_01/folder_1/2020'),
+                   f"{daily_folder}")
 
-    check_or_create_collab(box_folder=root_folder, box_user=service_user)
-
-    logger.info(f"Navigating Box folder path '{box_folder_path}'...")
-
-    box_root = box_client_app.folder('0').get()
-
-    logger.debug(f'Box root "{box_root.name}"')
-
-    parent = navigate(box_root, folder_list)
-
-    if not parent:
-        raise Exception(f'Daily folder "{daily_folder}" not found.')
-
-    with TemporaryDirectory() as tmp_dir:
-
-        logger.info(f'Downloading Box files from "{parent.name}"...')
-
-        local_paths = download_files(box_folder=parent, local_dir=tmp_dir)
-
-        logger.info(f'Uploading to ADLS "{upload_dir}"...')
-
-        upload_files(adls_client, adls_container_name, adls_dir=upload_dir,
-                     files=local_paths)
+    box_to_adls(box_client=box_client_app, source=box_dir,
+                adls_client=adls_service, adls_root=adls_container,
+                target=adls_dir)
 
     logger.info(f"Process complete...")
