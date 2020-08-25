31500710214be8e23ee94ad27fd8fcb0c8960325
shermanflan <shermanflan@gmail.com>
2020-08-24 21:43:54 -0500

    Refactored ETL class

--- Box2ADLS/box2adls/etl/__init__.py
+++ Box2ADLS/box2adls/etl/__init__.py
@@ -10,114 +10,118 @@ from box2adls.util.lake import upload_files
 from box2adls.util.xlsx import transform_daily, transform_weekly
 
 
-def daily_pull(box_client, source, source_mask, source_rename,
-               prev_label, curr_label, next_label,
-               lake_client, lake_root, target):
+class EtlOperations:
     """
-    Routine which pulls daily files from Box.
-
-    :param box_client: box client with access to source folder
-    :param source: box source path
-    :param source_mask: box file name filter
-    :param source_rename: box output file name
-    :param prev_label: New name of previous month tab
-    :param curr_label: New name of current month tab
-    :param next_label: New name of next month tab
-    :param lake_client: lake client
-    :param lake_root: lake container
-    :param target: lake target path
-    :return: None
+    Class encapsulating various ETL operations.
     """
-    today = date.today()
-    curr_month = date.strftime(today, '%B')
-    prev_month = today.replace(day=1) - timedelta(days=1)
-    prev_month = prev_month.strftime("%B")
+    def __init__(self, box_client, lake_client, lake_root, target):
+        """
+        Standard constructor.
 
-    if today.month == 12:
-        next_month = today.replace(year=today.year + 1, month=1)
-    else:
-        next_month = today.replace(month=today.month + 1, day=1)
+        :param box_client: box client with access to source folder
+        :param lake_client: lake client
+        :param lake_root: lake container
+        :param target: lake target path
+        """
+        self.box_client = box_client
+        self.lake_client = lake_client
+        self.lake_root = lake_root
+        self.lake_client = lake_client
+        self.target = target
 
-    next_month = next_month.strftime("%B")
+    def daily_pull(self, source, source_mask, source_rename, prev_label,
+                   curr_label, next_label):
+        """
+        Routine which pulls daily files from Box.
 
-    partial_transform = partial(transform_daily,
-                                prev_month=prev_month, prev_label=prev_label,
-                                curr_month=curr_month, curr_label=curr_label,
-                                next_month=next_month, next_label=next_label)
+        :param source: box source path
+        :param source_mask: box file name filter
+        :param source_rename: box output file name
+        :param prev_label: New name of previous month tab
+        :param curr_label: New name of current month tab
+        :param next_label: New name of next month tab
+        :return: None
+        """
+        today = date.today()
+        curr_month = date.strftime(today, '%B')
+        prev_month = today.replace(day=1) - timedelta(days=1)
+        prev_month = prev_month.strftime("%B")
 
-    box_to_lake(box_client, source, source_mask, source_rename,
-                lake_client, lake_root, target, partial_transform)
-
-
-def weekly_pull(box_client, source, source_mask, source_rename,
-                tab_name, tab_rename, lake_client, lake_root,
-                target):
-    """
-    Routine which pulls weekly files from Box.
-
-    :param box_client: box client with access to source folder
-    :param source: box source path
-    :param source_mask: box file name filter
-    :param source_rename: box output file name
-    :param tab_name: hidden tab name
-    :param tab_rename: new tab name
-    :param lake_client: lake client
-    :param lake_root: lake container
-    :param target: lake target path
-    :return: None
-    """
-    today = date.today()
-    last_friday = get_last_friday(today)
-    week_mask1 = date.strftime(last_friday, '%B - %d')
-    source_week = join(source, week_mask1)
-
-    partial_transform = partial(transform_weekly, tab_name=tab_name,
-                                tab_rename=tab_rename)
-
-    box_to_lake(box_client, source_week, source_mask, source_rename,
-                lake_client, lake_root, target, partial_transform)
-
-
-def box_to_lake(box_client, source, source_mask, source_rename,
-                lake_client, lake_root, target, transform):
-    """
-    Routine which copies folders from Box to ADLS.
-
-    :param box_client: box client with access to source folder
-    :param source: box source path
-    :param source_mask: box file name filter
-    :param source_rename: box output file name
-    :param lake_client: lake client
-    :param lake_root: lake container
-    :param target: lake target path
-    :param transform: file function handler
-    :return: None
-    """
-    box_root = box_client.folder('0').get()
-
-    logger.info(f"Navigating Box path '{source}' on '{box_root.name}'")
-
-    folders = source.split('/')[-1::-1]
-    box_folder = navigate(box_root, folders)
-
-    if not box_folder:
-        raise Exception(f'Box folder "{split(source)[1]}" not found.')
-
-    with TemporaryDirectory() as tmp_dir:
-
-        logger.info(f'Downloading Box files from "{box_folder.name}"...')
-
-        local_paths = download_file(box_folder=box_folder, file_mask=source_mask,
-                                    file_name=source_rename, local_dir=tmp_dir)
-
-        if local_paths:
-            for i in local_paths:
-                logger.info(f'Applying transformation to "{split(i)[1]}"...')
-                transform(source=i)
-
-            logger.info(f'Uploading to lake "{target}"...')
-
-            upload_files(lake_client, lake_root, lake_dir=target,
-                         files=local_paths)
+        if today.month == 12:
+            next_month = today.replace(year=today.year + 1, month=1)
         else:
-            logger.info(f'No Box files found in "{box_folder.name}"...')
+            next_month = today.replace(month=today.month + 1, day=1)
+
+        next_month = next_month.strftime("%B")
+
+        partial_transform = partial(transform_daily,
+                                    prev_month=prev_month,
+                                    prev_label=prev_label,
+                                    curr_month=curr_month,
+                                    curr_label=curr_label,
+                                    next_month=next_month,
+                                    next_label=next_label)
+
+        self.box_to_lake(source, source_mask, source_rename, partial_transform)
+
+    def weekly_pull(self, source, source_mask, source_rename, tab_name,
+                    tab_rename):
+        """
+        Routine which pulls weekly files from Box.
+
+        :param source: box source path
+        :param source_mask: box file name filter
+        :param source_rename: box output file name
+        :param tab_name: hidden tab name
+        :param tab_rename: new tab name
+        :return: None
+        """
+        today = date.today()
+        last_friday = get_last_friday(today)
+        week_mask1 = date.strftime(last_friday, '%B - %d')
+        source_week = join(source, week_mask1)
+
+        partial_transform = partial(transform_weekly, tab_name=tab_name,
+                                    tab_rename=tab_rename)
+
+        self.box_to_lake(source_week, source_mask, source_rename,
+                         partial_transform)
+
+    def box_to_lake(self, source, source_mask, source_rename, transform):
+        """
+        Routine which copies folders from Box to ADLS.
+
+        :param source: box source path
+        :param source_mask: box file name filter
+        :param source_rename: box output file name
+        :param transform: file function handler
+        :return: None
+        """
+        box_root = self.box_client.folder('0').get()
+
+        logger.info(f"Navigating Box path '{source}' on '{box_root.name}'")
+
+        folders = source.split('/')[-1::-1]
+        box_folder = navigate(box_root, folders)
+
+        if not box_folder:
+            raise Exception(f'Box folder "{split(source)[1]}" not found.')
+
+        with TemporaryDirectory() as tmp_dir:
+
+            logger.info(f'Downloading Box files from "{box_folder.name}"...')
+
+            local_paths = download_file(box_folder=box_folder, file_mask=source_mask,
+                                        file_name=source_rename, local_dir=tmp_dir)
+
+            if local_paths:
+                for i in local_paths:
+                    logger.info(f'Applying transformation to "{split(i)[1]}"...')
+                    transform(source=i)
+
+                logger.info(f'Uploading to lake "{self.target}"...')
+
+                upload_files(self.lake_client, self.lake_root, lake_dir=self.target,
+                             files=local_paths)
+            else:
+                logger.info(f'No Box files found in "{box_folder.name}"...')
--- Box2ADLS/main.py
+++ Box2ADLS/main.py
@@ -3,7 +3,7 @@ from os.path import join
 from azure.storage.filedatalake import DataLakeServiceClient
 
 from box2adls.auth.box_jwt import auth_jwt
-from box2adls.etl import daily_pull, weekly_pull
+from box2adls.etl import EtlOperations
 from box2adls.logging import root_logger as logger
 from box2adls import (BOX_USER, BOX_FOLDER,
                       BOX_PATH, BOX_PATH2,
@@ -33,32 +33,25 @@ if __name__ == '__main__':
                            box_service_client=box_client_app,
                            box_folder_id=BOX_FOLDER)
 
-    logger.debug(f"Box file mask: {BOX_MASK}")
-
     logger.info("Authenticating to lake...")
 
     lake_service = DataLakeServiceClient(account_url=LAKE_URL,
                                          credential=LAKE_KEY)
 
+    etl_ops = EtlOperations(box_client=box_client_app,
+                            lake_client=lake_service,
+                            lake_root=LAKE_CONTAINER, target=LAKE_PATH)
+
     logger.info("Pulling daily files...")
 
-    daily_pull(box_client=box_client_app,
-               source=BOX_PATH, source_mask=BOX_MASK,
-               source_rename=BOX_RENAME,
-               prev_label=TAB_NAME_PREV, curr_label=TAB_NAME_CURR,
-               next_label=TAB_NAME_NEXT,
-               lake_client=lake_service, lake_root=LAKE_CONTAINER,
-               target=LAKE_PATH)
-
-    logger.debug(f"Box file mask: {BOX_MASK2}")
+    etl_ops.daily_pull(source=BOX_PATH, source_mask=BOX_MASK,
+                       source_rename=BOX_RENAME, prev_label=TAB_NAME_PREV,
+                       curr_label=TAB_NAME_CURR, next_label=TAB_NAME_NEXT)
 
     logger.info("Pulling weekly files...")
 
-    weekly_pull(box_client=box_client_app,
-                source=BOX_PATH2, source_mask=BOX_MASK2,
-                source_rename=BOX_RENAME2, tab_name=TAB_HIDDEN_NAME,
-                tab_rename=TAB_HIDDEN_RENAME,
-                lake_client=lake_service, lake_root=LAKE_CONTAINER,
-                target=LAKE_PATH)
+    etl_ops.weekly_pull(source=BOX_PATH2, source_mask=BOX_MASK2,
+                        source_rename=BOX_RENAME2, tab_name=TAB_HIDDEN_NAME,
+                        tab_rename=TAB_HIDDEN_RENAME)
 
     logger.info(f"Process complete...")
