a78deaac54e20d4ee841af02b4c9991dffb25f98
shermanflan <shermanflan@gmail.com>
2020-10-02 15:09:00 -0500

    Working CLI version

--- cli/main.py
+++ cli/main.py
@@ -3,7 +3,9 @@ import logging
 from os import linesep
 from subprocess import run, CalledProcessError
 
-from utility import AZ_SUBSCRIPTION, AZ_RESOURCE_GROUP
+from utility import (AZ_SUBSCRIPTION, AZ_RESOURCE_GROUP,
+                     DIAGNOSTIC_NAME, AZ_LOG_WORKSPACE)
+from utility.cmd import add_diagnostic, get_by_tag, is_monitored
 from utility import log
 
 logger = logging.getLogger(__name__)
@@ -12,27 +14,8 @@ logger = logging.getLogger(__name__)
 # TODO: Run az login for delegated auth?
 if __name__ == '__main__':
 
-    factory_cmd = [
-        'az', 'resource', 'list',
-        '--resource-type', 'Microsoft.DataFactory/factories',
-        '--resource-group', AZ_RESOURCE_GROUP,
-        '--subscription', AZ_SUBSCRIPTION
-    ]
+    resources = get_by_tag(subscription=AZ_SUBSCRIPTION,
+                           resource_group=AZ_RESOURCE_GROUP,
+                           tag_name='auto-monitor')
 
-    try:
-        result = run(factory_cmd, shell=False,
-                     capture_output=True, check=True,
-                     encoding="utf-8", text=True)
-
-        trimmed = result.stdout[:result.stdout.rfind(']') + 1]
-        adf_map = json.loads(trimmed)
-        adf_names = [f['name'] for f in adf_map]
-
-        logger.debug(result.stdout)
-        logger.debug(f'Total: {len(adf_names)}')
-        logger.debug(linesep.join(adf_names))
-
-    except CalledProcessError as e:
-
-        logger.exception(e)
-        raise
+    filtered = [rid for rid, _ in resources if is_monitored(rid)]
---
+++ cli/utility/cmd.py
@@ -0,0 +1,172 @@
+import logging
+import json
+import os
+from subprocess import run, CalledProcessError
+
+from utility import LOG_MAP, METRIC_MAP
+from utility import log
+
+logger = logging.getLogger(__name__)
+
+
+def get_by_tag(subscription, tag_name, tag_value=None,
+               resource_group=None, resource_type=None):
+    """
+    Get resources by tag.
+
+    :param subscription:
+    :param resource_group:
+    :param resource_type:
+    :param tag_name:
+    :param tag_value:
+    :return: list of resource ids.
+    """
+    az_cmd = [
+        'az', 'resource', 'list',
+        '--subscription', subscription,
+    ]
+
+    if resource_group:
+        az_cmd.extend(['--resource-group', resource_group])
+
+    if resource_type:
+        az_cmd.extend(['--resource-type', resource_type])
+
+    try:
+        result = run(az_cmd, shell=False, capture_output=True, check=True,
+                     encoding="utf-8", text=True)
+
+        end_brace = max(result.stdout.rfind(']'), result.stdout.rfind('}'))
+        trimmed = result.stdout[:end_brace+1]
+        resources = json.loads(trimmed)
+        resource_ids = []
+
+        for f in resources:
+            if f['tags'] and tag_name in f['tags'] \
+                    and (not tag_value or tag_value == f['tags'][tag_name]):
+                resource_ids.append((f['id'], f['type']))
+
+        return resource_ids
+    except CalledProcessError as e:
+
+        logger.exception(e)
+        raise
+
+
+def is_monitored(resource_id):
+    """
+    Check if resource_id has at least one diagnostic setting.
+
+    # TODO:
+    Add diagnostic name filter
+
+    :param resource_id:
+    :return: True or False
+    """
+    az_cmd = [
+        'az', 'monitor', 'diagnostic-settings', 'list',
+        '--resource', resource_id,
+    ]
+
+    try:
+        result = run(az_cmd, shell=False, capture_output=True, check=True,
+                     encoding="utf-8", text=True)
+
+        end_brace = max(result.stdout.rfind(']'), result.stdout.rfind('}'))
+        trimmed = result.stdout[:end_brace+1]
+        resource = json.loads(trimmed)
+
+        return True if resource['value'] else False
+
+    except CalledProcessError as e:
+
+        logger.exception(e)
+        raise
+
+
+def remove_diagnostic(name, resource_id):
+    """
+
+    TODO:
+    Assert diagnostic exists
+
+    :param name:
+    :param resource_id:
+    :return:
+    """
+    try:
+        az_cmd = [
+            'az', 'monitor', 'diagnostic-settings', 'delete',
+            '--name', name,
+            '--resource', resource_id
+        ]
+
+        logger.debug(az_cmd)
+
+        run(az_cmd, check=True)
+
+        logger.debug(f'Removed {name} from {os.path.split(resource_id)[1]}.')
+
+    except CalledProcessError as e:
+
+        logger.exception(e)
+        raise
+
+
+def add_diagnostics(name, subscription, workspace_id, tag_name, tag_value):
+    """
+
+    :param name:
+    :param subscription:
+    :param workspace_id:
+    :param tag_name:
+    :param tag_value:
+    :return:
+    """
+    resources = get_by_tag(subscription, tag_name, tag_value)
+
+    logger.debug(f'Returned {len(resources)} resources.')
+
+    for rid, category in resources:
+        if not is_monitored(rid):
+            add_diagnostic(name, rid, category, workspace_id)
+
+
+def add_diagnostic(name, resource_id, category, workspace_id,
+                   log_map=LOG_MAP, metric_map=METRIC_MAP):
+    """
+
+    TODO:
+    Assert diagnostic does not exist.
+
+    :param name:
+    :param resource_id:
+    :param category:
+    :param workspace_id:
+    :param log_map:
+    :param metric_map:
+    :return:
+    """
+    az_cmd = [
+        'az', 'monitor', 'diagnostic-settings', 'create',
+        '--name', name,
+        '--resource', resource_id,
+        '--logs', f"{log_map[category]}",
+        '--metrics', f"{metric_map[category]}",
+        '--workspace', workspace_id,
+    ]
+
+    if category == 'Microsoft.DataFactory/factories':
+        az_cmd.extend(['--export-to-resource-specific', 'true'])
+
+    logger.debug(az_cmd)
+
+    try:
+        run(az_cmd, check=True)
+
+        logger.debug(f'Added {name} to {os.path.split(resource_id)[1]}.')
+
+    except CalledProcessError as e:
+
+        logger.exception(e)
+        raise
